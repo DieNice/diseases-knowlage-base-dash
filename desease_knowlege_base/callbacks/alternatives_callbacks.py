a208d248a0103b6cdb448f8a067146d6108770d2
DieNice <DailyPlanning@bk.ru>
2022-12-27 03:05:22 +1000

    WIP

--- desease_knowlege_base/callbacks/alternatives_callbacks.py
+++ desease_knowlege_base/callbacks/alternatives_callbacks.py
@@ -1,17 +1,17 @@
-from datetime import datetime
 from itertools import combinations
 from random import sample
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
+import dash as dcc
 import dash_core_components as dcc
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import sqlalchemy
 from app import app
-from callbacks.create_train import get_all_classes, get_all_train
 from config import dev_conn_settings
-from dash.exceptions import PreventUpdate
-from dash_extensions.enrich import Input, Output, State
+from dash import dash_table, html
+from dash_extensions.enrich import Input, Output
 from sqlalchemy import MetaData
 
 
@@ -200,7 +200,66 @@ def transform_df_batch_to_graph(df_batch: pd.DataFrame) -> dcc.Graph:
     return dcc.Graph(figure=fig)
 
 
-def generate_permutations(df_batch: pd.DataFrame) -> pd.DataFrame:
+def transform_df_batch_permutations_to_table(df_batch_permutations: List[pd.DataFrame]) -> dash_table.DataTable:
+    """Преобразование списка пачек с перестановками в Dash таблицу
+
+    Args:
+        df_batch_permutations (List[pd.DataFrame]): Список перестановок
+
+    Returns:
+        dash_table.DataTable: _description_
+    """
+
+    concated_df = pd.DataFrame()
+
+    for i, df in enumerate(df_batch_permutations):
+        df["num_comb"] = i + 1
+        concated_df = pd.concat([concated_df, df])
+    concated_df.drop(["uuid"], axis=1, inplace=True)
+
+    new_table = dash_table.DataTable(
+        data=concated_df.to_dict("records"),
+        columns=[{"id": "num_comb", "name": "Номер комбинации"},
+                 {"id": "desease", "name": "Название класса"},
+                 {"id": "history", "name": "Номер истории"},
+                 {"id": "num_period", "name": "Номер периода"},
+                 {"id": "moment_observation", "name": "Момент наблюдения"},
+                 {"id": "value", "name": "Значение периода динамики"},
+                 {"id": "duration", "name": "Длительность периода динамики"},
+                 ],
+        id="classes-tbl-id",
+        style_table={'height': '300px',
+                     'overflowY': 'auto', 'overflowX': 'auto'},
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(220, 220, 220)',
+            },
+            {'if': {'column_id': 'num_comb'},
+             'width': '15%'},
+            {'if': {'column_id': 'num_period'},
+             'width': '15%'},
+            {'if': {'column_id': 'value'},
+             'width': '20%'},
+            {'if': {'column_id': 'history'},
+             'width': '20%'},
+            {'if': {'column_id': 'duration'},
+             'width': '20%'},
+        ],
+        style_header={
+            'backgroundColor': 'rgb(210, 210, 210)',
+            'color': 'black',
+            'fontWeight': 'bold'
+        },
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        column_selectable='multi',
+    )
+    return html.Div(new_table)
+
+
+def generate_permutations(df_batch: pd.DataFrame) -> List[pd.DataFrame]:
     """Генерация расстановок
 
     Args:
@@ -209,15 +268,27 @@ def generate_permutations(df_batch: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Пачка данных с расстановками
     """
-    def map_moments_durations(moments: List, durations: List) -> List:
-        """Маппинг моментов наблюдения и сгенерированных периодов
+    def restore_durations_periods(moments: List, durations: List) -> Tuple[List, List]:
+        """Маппинг моментов наблюдения и сгенерированных периодов,
+        Правильная расстановка периодов, длительности периодов по комбинациям duriations
 
         Args:
             moments (List): Список моментов наблюдения
             durations (List): Список длительностей
         """
-
-        return []
+        result_durations = []
+        result_num_periods = []
+        moments_i = 0
+        len_moments = len(moments)
+        for num_period, duration in enumerate(durations):
+            for i in range(moments_i, len_moments):
+                if duration > moments[i]:
+                    result_durations.append(duration)
+                    result_num_periods.append(num_period+1)
+                    moments_i += 1
+        if len(result_durations) != len_moments:
+            raise Exception(f"Error restore durations for moments {moments}")
+        return result_durations, result_num_periods
 
     NUM_PERIODS = 5
 
@@ -249,10 +320,58 @@ def generate_permutations(df_batch: pd.DataFrame) -> pd.DataFrame:
                 new_all_combs.append(sorted(list(new_comb)))
     del all_combs
 
+    result_duration_batch_combs = []
     for comb in new_all_combs:
-        map_moments_durations(moments, comb)
+        restored_comb_durations, restored_periods = restore_durations_periods(
+            moments, comb)
+        new_batch = df_batch.copy()
+        new_batch.duration = np.array(restored_comb_durations)
+        new_batch.num_period = np.array(restored_periods)
+        result_duration_batch_combs.append(new_batch)
 
-    return new_all_combs
+    return result_duration_batch_combs
+
+
+def get_alternative(permutation: pd.DataFrame) -> pd.DataFrame:
+    """ На основе каждой расстановки границ периодов динамики
+    сформировать альтернативу индуктивной базы знаний (эта альтернатива
+    будет относиться к конкретному признаку из конкретной истории
+    болезни)
+     Условия формирования:
+        * для каждого периода динамики формируется значение параметра
+        «Значения для периода», в него включаются все неповторяющиеся
+        значения в моменты наблюдения, которые попали в этот период;
+        * для каждого периода динамики формируется значение параметра
+        «Нижняя граница» как разница первого момента наблюдения в
+        периоде и левой границы периода;
+        * для каждого периода динамики формируется значение параметра
+        «Верхняя граница» как разница последнего момента наблюдения в
+        периоде и левой границы периода.
+
+
+    Args:
+        permutations (List[pd.DataFrame]): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+
+    res_df = permutation.groupby('num_period').agg(
+        {'value': lambda x: list(x),
+         'moment_observation': min,
+         'duration': lambda x: x.iloc[0]
+         }).reset_index('num_period')
+
+    res_df.value = res_df.value.apply(lambda x: list(set(x)))
+
+    res_df.rename(columns={"moment_observation": "lower_duration",
+                  "duration": "upper_duration"}, inplace=True)
+    res_df["history"] = permutation.history[0]
+    res_df["desease"] = permutation.desease[0]
+    res_df["feature"] = permutation.feature[0]
+    res_df["amount_period"] = len(res_df.num_period.unique())
+
+    return res_df
 
 
 @ app.callback(
@@ -270,13 +389,27 @@ def generate_permutations(df_batch: pd.DataFrame) -> pd.DataFrame:
 def generate_alternatives(n_clicks: int) -> Dict:
     configs = generate_random_configs()
 
+    all_alternatives = pd.DataFrame()
     graphs = []
     for config in configs:
         batch = get_batch_by_config(config)
         df_batch = pd.DataFrame(batch)
         df_batch = preparing_df_batch(df_batch)
         graphs.append(transform_df_batch_to_graph(df_batch))
-        generate_permutations(df_batch)
+        permutations = generate_permutations(df_batch)
+
+        for permutation in permutations:
+            new_alternative = get_alternative(permutation)
+            all_alternatives = pd.concat([all_alternatives, new_alternative])
+
+        table = transform_df_batch_permutations_to_table(permutations)
+        graphs.append(table)
+
+    result = all_alternatives.groupby(['desease', 'feature', 'amount_period', 'num_period']).agg(
+        {'value': lambda x: set(list(sum(x, []))),
+         'lower_duration': min,
+         'upper_duration': max
+         }).reset_index(['desease', 'feature', 'amount_period', 'num_period'])
 
     return {"alert": "Generation successfull!",
             "graphs": graphs}
